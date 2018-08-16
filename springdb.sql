9eeb300e9f449d87a28e34a3b3fe97ebc40792f9
jagannath.p <jagan241296@gmail.com>
2018-08-16 20:51:45 +0530

    SQL dump added

---
+++ springdb.sql
@@ -0,0 +1,97 @@
+-- MySQL dump 10.13  Distrib 5.5.23, for Win64 (x86)
+--
+-- Host: localhost    Database: springboard
+-- ------------------------------------------------------
+-- Server version	5.5.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `assessment_table`
+--
+
+DROP TABLE IF EXISTS `assessment_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `assessment_table` (
+  `empcode` int(10) NOT NULL DEFAULT '0',
+  `subject_id` int(10) NOT NULL DEFAULT '0',
+  `empname` varchar(50) DEFAULT NULL,
+  `percent` int(10) DEFAULT NULL,
+  PRIMARY KEY (`empcode`,`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assessment_table`
+--
+
+LOCK TABLES `assessment_table` WRITE;
+/*!40000 ALTER TABLE `assessment_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assessment_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject_list`
+--
+
+DROP TABLE IF EXISTS `subject_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subject_list` (
+  `subject_id` int(10) NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject_list`
+--
+
+LOCK TABLES `subject_list` WRITE;
+/*!40000 ALTER TABLE `subject_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `training_material`
+--
+
+DROP TABLE IF EXISTS `training_material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `training_material` (
+  `subject_id` int(10) DEFAULT NULL,
+  `file_path` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `training_material`
+--
+
+LOCK TABLES `training_material` WRITE;
+/*!40000 ALTER TABLE `training_material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `training_material` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-16 20:50:52
