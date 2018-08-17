96c3fe6a8c5848f7e806583f885f000b1ebbf657
jagan241296 <jagan241296@gmail.com>
2018-08-17 22:25:46 +0530

    [HTTP][API]Reconfigured the Http API to support multiple requst types

--- springdb.sql
+++ springdb.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.23, for Win64 (x86)
+-- MySQL dump 10.13  Distrib 5.1.33, for Win32 (ia32)
 --
 -- Host: localhost    Database: springboard
 -- ------------------------------------------------------
--- Server version	5.5.23
+-- Server version	5.1.33-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -84,6 +84,35 @@ LOCK TABLES `training_material` WRITE;
 /*!40000 ALTER TABLE `training_material` DISABLE KEYS */;
 /*!40000 ALTER TABLE `training_material` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_details` (
+  `empcode` int(10) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `contact` bigint(11) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`empcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_details`
+--
+
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES (12345678,'jaggu','nagi2001@gmail.com',1111111111,'student','male','1bbd886460827015e5d605ed44252251');
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -94,4 +123,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-16 20:50:52
+-- Dump completed on 2018-08-17 16:54:07
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
-import {Http,Headers} from '@angular/http';
+import { Headers } from '@angular/http';
 import { Constants } from '../Constants';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-login',
@@ -9,40 +10,30 @@ import { Constants } from '../Constants';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm : FormGroup;
-  constructor(private http:Http) { }
-  errorMsg = "";
+  loginForm: FormGroup;
+  constructor(private httpService: HttpService) {}
   ngOnInit() {
     this.loginForm = new FormGroup({
-      'empcode' : new FormControl(null, [Validators.required,,this.invalidempcode.bind(this)]),
-      'password' : new FormControl(null, [Validators.required])
+      empcode: new FormControl(null, [
+        Validators.required,
+        this.invalidempcode.bind(this)
+      ]),
+      password: new FormControl(null, [Validators.required])
     });
   }
 
-  invalidempcode(control:FormControl):{[s:string]: boolean}{
-
-    if(isNaN(control.value)){
-      return {'empcodeisnotnum' : true};
+  invalidempcode(control: FormControl): { [s: string]: boolean } {
+    if (isNaN(control.value)) {
+      return { empcodeisnotnum: true };
     }
     return null;
-}
-
-  message;
-  onSubmit(){
-    
-    // if(this.loginForm.value.email === 'mishra.sagar25@gmail.com' && this.loginForm.value.password === 'sagar0987'){
-    //   alert('login');
-    //    document.getElementById("sp").style.display = "none";
-    //    this.loginForm.reset();
-    // }
-    // else{
-    //   this.errorMsg = "email or password is incorrect";
-    //   document.getElementById("sp").style.display = "flex "
-    // }
-    const heade = new Headers({ "Content-Type": "text-plain" });
-    this.http.post(Constants.LOGIN,this.loginForm.value,{headers : heade})
-      .subscribe((data)=>{this.message=data;console.log(this.message)});
   }
 
-
+  onSubmit() {
+    const header = new Headers({ 'Content-Type': 'text-plain' });
+    this.httpService.post_api(Constants.LOGIN, this.loginForm.value, header)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
 }
