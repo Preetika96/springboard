da7b69b7f4c4a5de3d5bfb1588ffe98a7690fb1e
vishalpatil937 <vishalpatil937@gmail.com>
2018-08-18 09:59:43 +0530

    [DB]User_details table included in dump

--- springdb.sql
+++ springdb.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.1.33, for Win32 (ia32)
+CREATE DATABASE  IF NOT EXISTS `springboard` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `springboard`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
 --
 -- Host: localhost    Database: springboard
 -- ------------------------------------------------------
--- Server version	5.1.33-community
+-- Server version	5.5.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +16,56 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `subject_list`
+--
+
+DROP TABLE IF EXISTS `subject_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subject_list` (
+  `subject_id` int(10) NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject_list`
+--
+
+LOCK TABLES `subject_list` WRITE;
+/*!40000 ALTER TABLE `subject_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_details` (
+  `empcode` int(11) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `contact` int(11) DEFAULT NULL,
+  `role` varchar(30) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_details`
+--
+
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `assessment_table`
@@ -41,29 +93,6 @@ LOCK TABLES `assessment_table` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `subject_list`
---
-
-DROP TABLE IF EXISTS `subject_list`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `subject_list` (
-  `subject_id` int(10) NOT NULL AUTO_INCREMENT,
-  `subject_name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`subject_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subject_list`
---
-
-LOCK TABLES `subject_list` WRITE;
-/*!40000 ALTER TABLE `subject_list` DISABLE KEYS */;
-/*!40000 ALTER TABLE `subject_list` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `training_material`
 --
 
@@ -84,35 +113,6 @@ LOCK TABLES `training_material` WRITE;
 /*!40000 ALTER TABLE `training_material` DISABLE KEYS */;
 /*!40000 ALTER TABLE `training_material` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user_details`
---
-
-DROP TABLE IF EXISTS `user_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_details` (
-  `empcode` int(10) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `contact` bigint(11) DEFAULT NULL,
-  `role` varchar(20) DEFAULT NULL,
-  `gender` varchar(10) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`empcode`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_details`
---
-
-LOCK TABLES `user_details` WRITE;
-/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
-INSERT INTO `user_details` VALUES (12345678,'jaggu','nagi2001@gmail.com',1111111111,'student','male','1bbd886460827015e5d605ed44252251');
-/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -123,4 +123,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-17 16:54:07
+-- Dump completed on 2018-08-18  9:58:08
