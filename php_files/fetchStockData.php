0a8c14cadaff6226bf67d0337e6e68737c8ef19a
jagan241296 <jagan241296@gmail.com>
2018-08-19 17:36:38 +0530

    [BACKEND][INTEGRATION]Casual bacend integration with all tabs in dashboard

---
+++ php_files/fetchStockData.php
@@ -0,0 +1,115 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    include 'Dbconfig.php';
+    
+    $received=json_decode(file_get_contents('php://input', true));
+    $request_type=$received->req_type;
+
+    //Switch case
+    switch($request_type)
+    {
+        case "1": 
+                {
+                    $result="";    
+                    //send SUbjects List
+                    Dbconfig::Init();
+                    $query="SELECT subject_name FROM subject_list";
+                    $return=Dbconfig::ReadTable($query);
+                    if ($return->num_rows > 0) {
+                        // output data of each row
+                        while($row = $return->fetch_assoc()) {
+                            $result .= $row["subject_name"] . ",";
+                        }
+                        echo $result;
+                    }
+                    Dbconfig::CloseConnection();
+                    break;
+                }
+        case "2": //Fetch students List
+                {         
+                    $result="";          
+                    //send SUbjects List
+                    Dbconfig::Init();
+                    $query="SELECT empname FROM assessment_table";
+                    $return=Dbconfig::ReadTable($query);
+                    if ($return->num_rows > 0) {
+                        // output data of each row
+                        while($row = $return->fetch_assoc()) {
+                             $result .= $row["empname"] . ",";
+                        }
+                        echo $result;
+                    }
+                    Dbconfig::CloseConnection();
+                    break;
+                }
+        case "3": //Show subjects
+                {         
+                    $result="";          
+                    //send SUbjects List
+                    Dbconfig::Init();
+                    $query="SELECT category, subject_name FROM subject_list";
+                    $return=Dbconfig::ReadTable($query);
+                    if ($return->num_rows > 0) {
+                        // output data of each row
+                        while($row = $return->fetch_assoc()) {
+                            if($result != "") {$result.= ",";}
+                             $result .= '{"name":"'.$row["subject_name"] . '",';
+                                $result .= '"category":"'.$row["category"] . '"}';
+                        }
+                        $result='{"records":['.$result.']}';
+                        echo $result;
+                    }
+                    Dbconfig::CloseConnection();
+                    break;
+                } 
+        case "4": //Show Students based on Subject
+                {         
+                    $subject_name = $received->subject_name;
+                    $result="";          
+                    //send SUbjects List
+                    Dbconfig::Init();
+                    $query="SELECT empcode, empname, percent FROM assessment_table WHERE subject_id = 
+                                                        (SELECT subject_id FROM subject_list WHERE subject_name = '$subject_name')";
+                    $return=Dbconfig::ReadTable($query);
+                    if ($return->num_rows > 0) {
+                        // output data of each row
+                        while($row = $return->fetch_assoc()) {
+                            if($result != "") {$result.= ",";}
+                            $result .= '{"empcode":"'.$row["empcode"] . '",';
+                             $result .= '"empname":"'.$row["empname"] . '",';
+                            $result .= '"percent":"'.$row["percent"] . '"}';
+                        }
+                        $result='{"records":['.$result.']}';
+                        echo $result;
+                    }
+                    Dbconfig::CloseConnection();
+                    break;
+                } 
+        case "5": //Show asessments based on student name
+                {         
+                    $student_name = $received->student_name;
+                    $result=""; 
+                    Dbconfig::Init();
+                    $query="SELECT subject_list.subject_name, assessment_table.percent 
+                                    FROM subject_list INNER JOIN assessment_table
+                                    ON subject_list.subject_id = assessment_table.subject_id 
+                                    AND assessment_table.empname = '$student_name'";
+
+                    $return=Dbconfig::ReadTable($query);
+                    if ($return->num_rows > 0) {
+                        // output data of each row
+                        while($row = $return->fetch_assoc()) {
+                            if($result != "") {$result.= ",";}
+                            $result .= '{"subject_name":"'.$row["subject_name"] . '",';                           
+                            $result .= '"percent":"'.$row["percent"] . '"}';
+                        }
+                        $result='{"records":['.$result.']}';
+                        echo $result;
+                    }
+                    Dbconfig::CloseConnection();
+                    break;
+                }               
+    }    
+?>
--- src/app/show-by-assessment/show-by-assessment.component.ts
+++ src/app/show-by-assessment/show-by-assessment.component.ts
@@ -1,5 +1,8 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Ng4LoadingSpinnerService } from '../../../node_modules/ng4-loading-spinner';
+import { Constants } from '../Constants';
 
 @Component({
   selector: 'app-show-by-assessment',
@@ -7,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-by-assessment.component.scss']
 })
 export class ShowByAssessmentComponent implements OnInit {
-  subjects = ['html','css'];
-  constructor(private router: Router, private route : ActivatedRoute) { }
+  subjects: string [];
+  obj: any;
+  response;
+  constructor(  private router: Router,
+                private route: ActivatedRoute,
+                private httpService: HttpService,
+                private spinnerService: Ng4LoadingSpinnerService) {}
 
   ngOnInit() {
+    // Fetch SUbjects List from DB
+    this.spinnerService.show();
+    this.obj =
+    {
+      'req_type': 1
+    }
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    // Create an Object of Header specifying the content type and pass it to the method
+  this.httpService.post_api(Constants.FETCH_STOCK_DATA, JSON.stringify(this.obj), headers)
+    .subscribe(data => {
+      this.response = data;
+      this.subjects = this.response._body.split(',');
+      this.subjects.pop();
+      this.spinnerService.hide();
+    });
   }
 
   showStudent(subject){
